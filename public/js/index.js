a10dd865bf399f83cdccabd99d518fcf98e651c6
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-04-25 21:20:15 +0800

    add scroll to bottom function

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+const scrollToBottom = () => {
+    // Selectors
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight  = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 socket.on('connect', () => {
     console.log('connected to server');
 });
@@ -18,6 +34,7 @@ socket.on('newMessage', (msg) => {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', (msg) => {
@@ -30,6 +47,7 @@ socket.on('newLocationMessage', (msg) => {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', (e) => {
